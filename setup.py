e6353699bab45dd8127e8081a33dfb264b368989
ben105 <ben_rooke@icloud.com>
2021-05-22 10:00:24 -0700

    Fix setup to not rewrite entire config.

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ import sys
 
 CONFIG_PATH = 'config.json'
 
+config = {}
+with open(CONFIG_PATH) as config_file:
+    config = json.load(config_file)
+
 path_to_excel_file = input('Drag the Excel file you wish to read from here: ')
-config = {
-    'excel_path': path_to_excel_file.strip("'").strip('"').strip()
-}
+
+config['excel_path'] = path_to_excel_file.strip("'").strip('"').strip().replace('\\', '')
 
 with open(CONFIG_PATH, 'w') as config_file:
     json.dump(config, config_file)
