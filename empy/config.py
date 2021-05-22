e06b7ed22b6637d991e294d0cfc2e2034789adf8
ben105 <ben_rooke@icloud.com>
2021-05-22 11:59:42 -0700

    Update setup process.

--- empy/config.py
+++ empy/config.py
@@ -8,6 +8,11 @@ class Config:
     hours_sheet = ''
     days_sheet = ''
 
+    peak_rates = 0
+    partial_peak = 0
+    off_peak = 0.18
+    super_off_peak = 0.18
+
     @staticmethod
     def initialize():
         with open('config.json') as config_file:
@@ -17,3 +22,7 @@ class Config:
             Config.minutes_sheet = config_json['minutes_sheet']
             Config.hours_sheet = config_json['hours_sheet']
             Config.days_sheet = config_json['days_sheet']
+            Config.peak_rates = config_json['rates']['peak']
+            Config.partial_peak = config_json['rates']['partial_peak']
+            Config.off_peak = config_json['rates']['off_peak']
+            Config.super_off_peak = config_json['rates']['super_off_peak']
--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ path_to_excel_file = input('Drag the Excel file you wish to read from here: ')
 config['excel_path'] = path_to_excel_file.strip("'").strip('"').strip().replace('\\', '')
 
 with open(CONFIG_PATH, 'w') as config_file:
-    json.dump(config, config_file)
+    json.dump(config, config_file, indent=4, sort_keys=True)
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
